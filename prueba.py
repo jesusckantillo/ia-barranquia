e171cabcc8c70c1d6088753c6a9e6f6cb27a07da
albertojs7 <131180244+albertojs7@users.noreply.github.com>
2024-05-04 17:04:28 -0500

    limpieza

--- prueba.py
+++ prueba.py
@@ -7,8 +7,8 @@ import seaborn as sns
 sns.set() # Use seaborn's default style to make attractive graphs
 plt.rcParams['figure.dpi'] = 100 # Show nicely large images in this notebook
 
-snd1 = parselmouth.Sound("xd11.wav")
-snd2 = parselmouth.Sound("xd12.wav")
+snd1 = parselmouth.Sound("botbueno.wav")
+snd2 = parselmouth.Sound("xd.wav")
 def draw_spectrogram(spectrogram, dynamic_range=70):
     X, Y = spectrogram.x_grid(), spectrogram.y_grid()
     sg_db = 10 * np.log10(spectrogram.values)
@@ -18,14 +18,18 @@ def draw_spectrogram(spectrogram, dynamic_range=70):
     plt.ylabel("frequency [Hz]")
 
 def draw_intensity(intensity):
-    plt.plot(intensity.xs(), intensity.values.T, linewidth=3, color='w')
-    plt.plot(intensity.xs(), intensity.values.T, linewidth=1)
+
+    limpio = [i if i >= 60 else 0 for i in intensity.values[0]]
+    print(limpio)
+    plt.plot(intensity.xs(), limpio, linewidth=3, color='w')
+    plt.plot(intensity.xs(), limpio, linewidth=1)
     plt.grid(False)
     plt.ylim(0)
     plt.ylabel("intensity [dB]")
 
 intensity = snd1.to_intensity()
 spectrogram = snd1.to_spectrogram()
+
 plt.figure()
 draw_spectrogram(spectrogram)
 plt.twinx()
