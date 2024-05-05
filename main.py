27e48f2ac67ad156421c234d72dac5c8d7ed62d4
Amdres <japovea@uninorte.edu.co>
2024-05-05 04:33:48 -0500

    Added cors support

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers import text_routes
 
-#
 app = FastAPI()
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(text_routes.router)
 @app.get("/")
 def read_root():
